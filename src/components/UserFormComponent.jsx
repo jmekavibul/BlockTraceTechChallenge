a684915ee654dbe1592ef0c7a2dcaad59dcae76a
James Mekavibul <31485121+jmekavibul@users.noreply.github.com>
2023-05-16 22:26:26 -0400

    add some URL

--- src/components/UserFormComponent.jsx
+++ src/components/UserFormComponent.jsx
@@ -36,43 +36,54 @@ const UserFormComponent = () => {
 
   return (
     <div className="form-container">
-      <form onSubmit={handleSubmit}> 
-        <div>
-          <h3>Contact Form</h3>
+      <form onSubmit={handleSubmit} className="my-6 space-y-8 w-1/3 mx-auto"> 
+        <div>      
+          <label class="block text-gray-700 text-lg font-bold mb-2" for="username">
+            BlockTrace Tech Challenge
+          </label>
         </div>
         <div>
-          <h5>Crypto Addresses</h5>
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Crypto Addresses
+          </label>
           <textarea
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             name="address"
             value={cryptoTransaction.address}
             placeholder="PlaceHolder"
             onChange={handleChange}
-            ></textarea>
+          />
         </div>
         <div>
-          <h5>API Key</h5>
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            API Key
+          </label>
           <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             name="apiKey"
             placeholder="API Key"
             value={cryptoTransaction.apiKey}
             onChange={handleChange}
-            
-            />
+          />
         </div>
         <div>
-          <h5>Crypto Address Type</h5>
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Crypto Address Type
+          </label>
           <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type="text"
             name="addressType"
             placeholder="Crypto Address Type"
             value={cryptoTransaction.addressType}
             onChange={handleChange}
-
           />
         </div>
         <div>
-          <button>Submit Contact</button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Submit Contact
+        </button>
         </div>
         {data && <TableComponent data={data} />}
         {responseMessage && <div>{responseMessage}</div>} {/* Display response message if available */}
